49222c8a4e36a89793a012322f2c6d6876498168
santupatil081 <santupatil081@gmail.com>
2022-07-25 11:44:39 +0530

    first commit

---
+++ src/test/java/com/TYSS/MyFirstGitCommandProjectTest.java
@@ -0,0 +1,11 @@
+package com.TYSS;
+
+import org.testng.annotations.Test;
+
+public class MyFirstGitCommandProjectTest {
+	@Test
+void commandGit()
+{
+System.out.println("This is a first git command program");	
+}
+}
